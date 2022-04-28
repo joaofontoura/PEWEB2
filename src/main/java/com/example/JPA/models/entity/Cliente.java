66388375ef41291f4b115fd7b2eb911ba953471d
joaofontoura <56686188+joaofontoura@users.noreply.github.com>
2022-04-28 11:18:50 -0300

    Add files via upload

---
+++ src/main/java/com/example/JPA/models/entity/Cliente.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.JPA.models.entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author labbit-03
+ */
+
+@Entity
+@Table(name = "tb_cliente")
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "tipo")
+
+public abstract class Cliente implements Serializable{
+    
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Venda> vendas;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Venda> getVendas() {
+        return vendas;
+    }
+
+    public void setVendas(List<Venda> vendas) {
+        this.vendas = vendas;
+    }   
+    
+}
